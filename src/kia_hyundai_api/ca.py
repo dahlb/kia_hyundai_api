f46191a72dd1fad0e8ad3baffd64c7db075eb110
Brendan Dahl <dahl.brendan@gmail.com>
2021-12-19 13:49:50 -0500

    chore: scraped sample responses from past community issues

--- src/kia_hyundai_api/ca.py
+++ src/kia_hyundai_api/ca.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from aiohttp import ClientSession, ClientResponse, ClientError
 
 from .auth_error import AuthError
+from .rate_error import RateError
 from .const import CA_TEMP_RANGE
 
 _LOGGER = logging.getLogger(__name__)
@@ -18,6 +19,11 @@ def convert_set_temp_to_api_from_celsius_value(set_temp):
 
 
 def request_with_logging_and_errors_raised(func):
+    """
+    {'error': {'errorCode': '6533', 'errorDesc': 'We are currently processing an earlier inquiry for your vehicle. Please wait 90 seconds before issuing next request.'}, 'responseHeader': {'responseCode': 1, 'responseDesc': 'Failure'}}
+    {'error': {'errorCode': '7310', 'errorDesc': 'PIN incorrect. Multiple incorrect attempts will require PIN reset'}, 'responseHeader': {'responseCode': 1, 'responseDesc': 'Failure'}}
+    {'error': {'errorCode': '6534', 'errorDesc': 'We apologize, but your daily limit of remote services requests has been exceeded. Your request for <<service name>> was not processed. Please try again tomorrow.'}, 'responseHeader': {'responseCode': 1, 'responseDesc': 'Failure'}}
+    """
     async def request_with_logging_wrapper(*args, **kwargs):
         url = kwargs["url"]
         json_body = kwargs.get("json_body")
@@ -33,9 +39,14 @@ def request_with_logging_and_errors_raised(func):
         if response_json["responseHeader"]["responseCode"] == 0:
             return response
         # still need to add error code for expired token
-        if response_json["error"]["errorCode"] == 7404:
-            _LOGGER.debug(f"invalid password")
+        if response_json["error"]["errorCode"] == "7404":
+            _LOGGER.error(f"invalid password")
             raise AuthError(response_json["error"]["errorDesc"])
+        if response_json["error"]["errorCode"] == "7310":
+            _LOGGER.error(f"invalid pin")
+            raise AuthError(response_json["error"]["errorDesc"])
+        if response_json["error"]["errorCode"] in ["6533", "6534"]:
+            raise RateError(response_json["error"]["errorDesc"])
         response_text = await response.text()
         _LOGGER.error(f"error: unknown error response {response_text}")
         raise ClientError
@@ -128,6 +139,9 @@ class Ca(ABC):
         return {"access_token": access_token, "refresh_token": refresh_token}
 
     async def get_vehicles(self, access_token: str):
+        """
+        {"responseHeader":{"responseCode":0,"responseDesc":"Success"},"result":{"vehicles":[{"vehicleId":"ID","nickName":"Optimus","modelCode":"D5S6D2A1FG","modelName":"Optima","modelYear":"2019","fuelKindCode":"G","trim":"","engine":"","exteriorColor":"M5U","dtcCount":0,"subscriptionStatus":"A","subscriptionEndDate":"20240422","mileageForNextService":-43557.8,"daysForNextService":"20200427","overviewMessage":"","defaultVehicle":true,"enrollmentStatus":"4","genType":"G1","vin":"7986"}]}}
+        """
         url = self.api_url + "vhcllst"
         response: ClientResponse = (
             await self._post_request_with_logging_and_errors_raised(
@@ -138,6 +152,12 @@ class Ca(ABC):
         return response_json["result"]
 
     async def get_cached_vehicle_status(self, access_token: str, vehicle_id: str):
+        """
+        {'responseHeader': {'responseCode': 0, 'responseDesc': 'Success'}, 'result': {'status': {'lastStatusDate': '20211119145414', 'airCtrlOn': False, 'engine': False, 'doorLock': True, 'doorOpen': {'frontLeft': 0, 'frontRight': 0, 'backLeft': 0, 'backRight': 0}, 'trunkOpen': False, 'airTempUnit': 'C', 'airTemp': {'value': '01H', 'unit': 0}, 'defrost': False, 'lowFuelLight': False, 'acc': False, 'hoodOpen': False, 'transCond': True, 'steerWheelHeat': 0, 'sideBackWindowHeat': 0, 'dte': {'value': 416.0, 'unit': 1}, 'tirePressureLamp': {'tirePressureLampAll': 0, 'tirePressureLampFL': 0, 'tirePressureLampFR': 0, 'tirePressureLampRL': 0, 'tirePressureLampRR': 0}, 'battery': {'batSoc': 81, 'batState': 0, 'sjbDeliveryMode': 0, 'batSignalReferenceValue': {}}, 'remoteIgnition': True, 'seatHeaterVentInfo': {'drvSeatHeatState': 2, 'astSeatHeatState': 2, 'rlSeatHeatState': 2, 'rrSeatHeatState': 2}, 'sleepModeCheck': True, 'lampWireStatus': {'headLamp': {}, 'stopLamp': {}, 'turnSignalLamp': {}}, 'windowOpen': {}, 'engineOilStatus': False, 'vehicleMovementHis': True, 'engineRuntime': {'value': 29, 'unit': 1}}}}
+        {'responseHeader': {'responseCode': 0, 'responseDesc': 'Success'}, 'result': {'status': {'lastStatusDate': '20210801171424', 'airCtrlOn': False, 'engine': False, 'doorLock': False, 'doorOpen': {'frontLeft': 0, 'frontRight': 0, 'backLeft': 0, 'backRight': 0}, 'trunkOpen': False, 'airTempUnit': 'C', 'airTemp': {'value': '01H', 'unit': 0}, 'defrost': False, 'lowFuelLight': False, 'acc': False, 'hoodOpen': False, 'transCond': True, 'dte': {'unit': 1}, 'tirePressureLamp': {}, 'battery': {'batSoc': 77, 'batSignalReferenceValue': {}}, 'remoteIgnition': True, 'seatHeaterVentInfo': {}, 'sleepModeCheck': True, 'lampWireStatus': {'headLamp': {}, 'stopLamp': {}, 'turnSignalLamp': {}}, 'windowOpen': {}, 'engineRuntime': {}}}}
+        {'responseHeader': {'responseCode': 0, 'responseDesc': 'Success'}, 'result': {'status': {'lastStatusDate': '20210920003648', 'airCtrlOn': False, 'engine': False, 'doorLock': False, 'doorOpen': {'frontLeft': 0, 'frontRight': 0, 'backLeft': 0, 'backRight': 0}, 'trunkOpen': False, 'airTempUnit': 'C', 'airTemp': {'value': '01H', 'unit': 0}, 'defrost': False, 'lowFuelLight': False, 'acc': False, 'hoodOpen': False, 'transCond': True, 'steerWheelHeat': 0, 'sideBackWindowHeat': 0, 'dte': {}, 'tirePressureLamp': {}, 'battery': {'batSoc': 81, 'batSignalReferenceValue': {}}, 'remoteIgnition': True, 'seatHeaterVentInfo': {}, 'sleepModeCheck': True, 'lampWireStatus': {'headLamp': {}, 'stopLamp': {}, 'turnSignalLamp': {}}, 'windowOpen': {}, 'engineRuntime': {}}}}
+        {'responseHeader': {'responseCode': 0, 'responseDesc': 'Success'}, 'result': {'status': {'lastStatusDate': '20211116153356', 'airCtrlOn': False, 'engine': False, 'doorLock': True, 'doorOpen': {'frontLeft': 0, 'frontRight': 0, 'backLeft': 0, 'backRight': 0}, 'trunkOpen': False, 'airTempUnit': 'C', 'airTemp': {'value': '00H', 'unit': 0}, 'defrost': False, 'acc': False, 'evStatus': {'batteryCharge': False, 'batteryStatus': 29, 'batteryPlugin': 0, 'remainTime2': { 'etc1': { 'value': 0, 'unit': 1 }, 'etc2': { 'value': 0, 'unit': 1 }, 'etc3': { 'value': 0, 'unit': 1 }, 'atc': { 'value': 0, 'unit': 1 } }, 'drvDistance': [ { 'rangeByFuel': { 'gasModeRange': { 'value': 0.0, 'unit': 1 }, 'evModeRange': { 'value': 104.0, 'unit': 1 }, 'totalAvailableRange': { 'value': 104.0, 'unit': 1 } }, 'type': 2 } ] }, 'hoodOpen': False, 'transCond': True, 'steerWheelHeat': 0, 'sideBackWindowHeat': 0, 'dte': { }, 'tirePressureLamp': { 'tirePressureLampAll': 0 }, 'battery': { 'batSoc': 87, 'batState': 0, 'batSignalReferenceValue': { } }, 'remoteIgnition': True, 'seatHeaterVentInfo': { }, 'sleepModeCheck': False, 'lampWireStatus': { 'headLamp': { }, 'stopLamp': { }, 'turnSignalLamp': { } }, 'windowOpen': { }, 'engineRuntime': { } } } }
+        """
         url = self.api_url + "lstvhclsts"
         response = await self._post_request_with_logging_and_errors_raised(
             access_token=access_token, vehicle_id=vehicle_id, url=url
@@ -146,6 +166,11 @@ class Ca(ABC):
         return response_json["result"]
 
     async def get_next_service_status(self, access_token: str, vehicle_id: str):
+        """
+        { "responseHeader": { "responseCode": 0, "responseDesc": "Success" }, "result": { "maintenanceInfo": { "msopServiceOdometer": 36057.0, "msopServiceOdometerUnit": 1, "mtspServiceDate": "20210721", "imatServiceOdometer": 48057.0, "imatServiceOdometerUnit": 1, "mtitServiceDate": "20220121", "currentOdometer": 37236.8, "currentOdometerUnit": 1, "serviceOdometerDuration": 12000.0, "serviceDaysDuration": 184, "serviceMonthsThreshold": 6 } } }
+        { 'responseHeader': { 'responseCode': 0, 'responseDesc': 'Success' }, 'result': { 'maintenanceInfo': { 'mtspServiceDate': '20190427', 'imatServiceOdometer': 12000.0, 'imatServiceOdometerUnit': 1, 'mtitServiceDate': '20200427', 'currentOdometer': 58322.9, 'currentOdometerUnit': 1, 'serviceOdometerDuration': 12000.0, 'serviceDaysDuration': 366, 'serviceMonthsThreshold': 12 } } }
+        { 'responseHeader': { 'responseCode': 0, 'responseDesc': 'Success' }, 'result': { 'maintenanceInfo': { 'mtspServiceDate': '20210917', 'imatServiceOdometer': 6000.0, 'imatServiceOdometerUnit': 1, 'mtitServiceDate': '20220317', 'currentOdometer': 2213.899902, 'currentOdometerUnit': 1, 'serviceOdometerDuration': 6000.0, 'serviceDaysDuration': 181, 'serviceMonthsThreshold': 6 } } }
+        """
         url = self.api_url + "nxtsvc"
         response = await self._post_request_with_logging_and_errors_raised(
             access_token=access_token, vehicle_id=vehicle_id, url=url
@@ -178,6 +203,9 @@ class Ca(ABC):
         return response_json["result"]
 
     async def request_vehicle_data_sync(self, access_token: str, vehicle_id: str):
+        """
+        {'responseHeader': {'responseCode': 0, 'responseDesc': 'Success'}, 'result': {'status': {'lastStatusDate': '20211119221003', 'airCtrlOn': False, 'engine': False, 'doorLock': True, 'doorOpen': {'frontLeft': 0, 'frontRight': 0, 'backLeft': 0, 'backRight': 0}, 'trunkOpen': False, 'airTempUnit': 'C', 'airTemp': {'value': '01H', 'unit': 0}, 'defrost': False, 'lowFuelLight': False, 'acc': False, 'hoodOpen': False, 'transCond': True, 'steerWheelHeat': 0, 'sideBackWindowHeat': 0, 'dte': {'value': 416.0, 'unit': 1}, 'tirePressureLamp': {'tirePressureLampAll': 0, 'tirePressureLampFL': 0, 'tirePressureLampFR': 0, 'tirePressureLampRL': 0, 'tirePressureLampRR': 0}, 'battery': {'batSoc': 81, 'batState': 0, 'sjbDeliveryMode': 0, 'batSignalReferenceValue': {}}, 'remoteIgnition': True, 'seatHeaterVentInfo': {'drvSeatHeatState': 2, 'astSeatHeatState': 2, 'rlSeatHeatState': 2, 'rrSeatHeatState': 2}, 'lampWireStatus': {'headLamp': {}, 'stopLamp': {}, 'turnSignalLamp': {}}, 'windowOpen': {}, 'engineOilStatus': False, 'vehicleMovementHis': False, 'engineRuntime': {'value': 0, 'unit': 1}}}}
+        """
         url = self.api_url + "rltmvhclsts"
         await self._post_request_with_logging_and_errors_raised(
             access_token=access_token, vehicle_id=vehicle_id, url=url
@@ -210,6 +238,9 @@ class Ca(ABC):
     async def lock(
         self, access_token: str, vehicle_id: str, pin: str, pin_token: str = None
     ) -> str:
+        """
+        {'responseHeader': {'responseCode': 0, 'responseDesc': 'Success'}}
+        """
         url = self.api_url + "drlck"
         json_body = {"pin": pin}
         response = await self._post_request_with_logging_and_errors_raised(
